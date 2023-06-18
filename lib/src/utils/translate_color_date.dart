7e36f1b33c69402821894d0cc605989e9fdaa54c
Aditya Rajput <adiraj20072002@gmail.com>
2023-06-18 13:47:26 +0530

    feat: Implement translation for `AwesomeDatePickerMode.hex`

--- lib/src/utils/translate_color_date.dart
+++ lib/src/utils/translate_color_date.dart
@@ -8,7 +8,20 @@ extension Calendar on Color {
   DateTime toDate({
     required AwesomeDatePickerMode mode,
     required bool enableAlpha,
-  }) {}
+  }) {
+    switch (mode) {
+      case AwesomeDatePickerMode.hex:
+        return DateTime.fromMillisecondsSinceEpoch(
+          value * DateTime(3000).millisecondsSinceEpoch ~/ 0xFFFFFFFF,
+        );
+      case AwesomeDatePickerMode.hsv:
+      case AwesomeDatePickerMode.rgb:
+      case AwesomeDatePickerMode.hsva:
+      case AwesomeDatePickerMode.rgba:
+        // TODO: Handle these cases
+        return DateTime(2002);
+    }
+  }
 }
 
 extension Calendr on HSVColor {
@@ -26,7 +39,22 @@ extension Palette on DateTime {
   HSVColor toHSVColor({
     required AwesomeDatePickerMode mode,
     required bool enableAlpha,
-  }) {}
+  }) {
+    switch (mode) {
+      case AwesomeDatePickerMode.hex:
+        return Color(
+          millisecondsSinceEpoch *
+              0xFFFFFFFF ~/
+              DateTime(3000).millisecondsSinceEpoch,
+        ).toHSVColor();
+      case AwesomeDatePickerMode.hsv:
+      case AwesomeDatePickerMode.rgb:
+      case AwesomeDatePickerMode.hsva:
+      case AwesomeDatePickerMode.rgba:
+        // TODO: Handle these cases
+        return HSVColor.fromColor(Colors.cyan);
+    }
+  }
 
   Color toColor({
     required AwesomeDatePickerMode mode,
